cdb4c4c524c11954f1ae4cbe9db59c945b663b8f
José Alberto <j.alberto_mp@outlook.com>
2023-05-04 23:52:00 -0600

    Basic structure for the project

---
+++ DynamicConnection.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DynamicConnection.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
